e6e53230e9bf3c26f784bdc4640cc4151f5d8eeb
Ivan Riazantsev <ivan.riazantsev@gmail.com>
2019-12-18 07:53:57 +0200

    Update PresenceService.js

--- src/service/PresenceService.js
+++ src/service/PresenceService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_OFFICES_URL = `http://localhost:8080/presence`;
+const BASE_OFFICES_URL = `https://prodman-frontend.herokuapp.com/presence`;
 
 export function getPresenceForWeek(id) {
     return axios.get(`${BASE_OFFICES_URL}/${id}/day`).then(res => res.data);
